88c4326b059b93b1d56c75702b3a6917c90b0490
Robert M Ochshorn <mail@RMOZONE.COM>
2012-08-20 21:12:36 -0700

    clustering and cluster serialization

---
+++ cluster.py
@@ -0,0 +1,81 @@
+# Combine `feat` files and cluster into 15 bins.
+
+import numpy as np
+from scipy.cluster.vq import kmeans, vq
+import json
+import numm
+
+import meap
+
+
+def cluster(src, key="AvgTonalCentroid(6)", nbins=15, min_volume=-30):
+    analyses = meap.analysis(src)
+
+    segs = []
+    features = []
+    rms = []
+    for idx, analysis in enumerate(analyses):
+        for X in analysis:
+            segs.append((X["onset_time"] + 300 * idx, X["chunk_length"]))
+            features.append(X[key])
+            rms.append(X["RMSAmplitude(1)"])
+
+    rms = np.array(rms)
+    features = np.array(features)
+    segs = np.array(segs)
+
+    too_quiet = rms < min_volume
+
+    quiet_segs = segs[too_quiet]
+
+    features = features[np.invert(too_quiet)]
+    segs = segs[np.invert(too_quiet)]
+
+    codebook, distortion = kmeans(features, nbins)
+
+    clusters, distance = vq(features, codebook)
+
+    out = {}
+    for i in range(nbins + 1):
+        out[i] = []
+
+    for idx, cluster in enumerate(clusters):
+        out[cluster].append(segs[idx].tolist())
+
+    out[nbins] = quiet_segs.tolist()
+    
+    return out
+
+def serialize(clusters, filename):
+    json.dump(clusters, open(filename, 'w'))
+
+def wave(src_np, clusters, outpattern, R=44100):
+    for idx, segs in clusters.items():
+        segchunks = [src_np[int(R*st):int(R*(st+dur))] for (st, dur) in segs]
+        if len(segchunks) == 0:
+            print 'zero-length cluster', idx
+            continue
+        segchunks = np.concatenate(segchunks)
+        numm.np2sound(segchunks, outpattern % (idx))
+
+if __name__=='__main__':
+    import sys
+    import os
+
+    keys = ["AvgTonalCentroid(6)", "AvgMFCC(13)", "AvgChroma(12)"]
+    nclusters = 36
+
+    for src in sys.argv[1:]:
+        src_np = None
+
+        for key in keys:
+            jsonfile = "%s-%s-%d.json" % (src, key, nclusters)
+            if os.path.exists(jsonfile):
+                continue
+
+            if src_np is None:
+                src_np = numm.sound2np(src)
+
+            clusters = cluster(src, key=key, nbins=nclusters)
+            serialize(clusters, jsonfile)
+            wave(src_np, clusters, "%s-%s-%d-%%06d.wav" % (src, key, nclusters))
