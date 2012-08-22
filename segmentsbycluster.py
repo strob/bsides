91fcb0d2c583d2313c2e12879aaecfa7312c317f
Robert M Ochshorn <mail@RMOZONE.COM>
2012-08-21 17:59:44 -0700

    audio_out

--- segmentsbycluster.py
+++ segmentsbycluster.py
@@ -5,15 +5,42 @@ import numpy as np
 
 cur_cluster = '0'
 cur_idx = 0
+cur_frame = 0
 
 def keyboard_in(type, button):
-    global cur_cluster
+    global cur_cluster, cur_idx, cur_frame
     if len(button) == 1:
         cur_cluster = button.upper()
 
-def video_out(a):
-    cv2.putText(a, cur_cluster, (10,20), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,0))
+        cur_idx = 0
+        cur_frame = 0
 
+def mouse_in(type, px, py, button):
+    global cur_idx, cur_frame
+    
+    nsegs = len(clusters[cur_cluster])
+    N = int(np.ceil(np.sqrt(nsegs)))
+
+    _oidx = cur_idx
+    cur_idx = min(nsegs-1, int(px*N) + N*int(py*N))
+
+    if cur_idx != _oidx:
+        cur_frame = 0
+
+def audio_out(a):
+    global cur_frame
+    seg = clusters[cur_cluster][cur_idx]
+    segarr = arr[seg.st_idx:seg.end_idx]
+
+    if len(segarr) < len(a):
+        print 'warning: very short segment'
+        a[:len(segarr)] = segarr
+        return
+
+    a[:] = np.roll(segarr,-cur_frame,axis=0)[:len(a)]
+    cur_frame = (cur_frame + len(a)) % (len(segarr))
+
+def video_out(a):
     # draw a grid of segment circles
     segs = clusters[cur_cluster]
     N = int(np.ceil(np.sqrt(len(segs))))
@@ -25,8 +52,17 @@ def video_out(a):
         row = int(i / N)
         col = i % N
 
-        cv2.circle(a, (col*stepx + r, row*stepy + r), r, (255,0,0))
-        
+        color = (255,255,255)
+        stroke = 1
+        if i == cur_idx:
+            color = (255,0,0)
+            stroke = -1
+
+        x,y = (col*stepx + r, row*stepy + r)
+        cv2.circle(a, (x,y), r, color, stroke)
+        cv2.putText(a, str(i), (x,y), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
+
+    cv2.putText(a, cur_cluster, (305,230), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,0))
 
 if __name__=='__main__':
     import sys
