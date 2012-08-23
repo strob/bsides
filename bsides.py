9edaf1ed50a40705e29d0e13d1e426f30c409556
Robert M Ochshorn <mail@RMOZONE.COM>
2012-08-23 16:44:01 -0700

    delete

--- bsides.py
+++ bsides.py
@@ -184,6 +184,7 @@ def rhythm_keys(type, button):
             zoom_idx = ZOOM_LEVELS.index('sound')
         elif button == 'Escape':
             zoom_idx = ZOOM_LEVELS.index('structure')
+
 def sound_keys(type, button):
     global sound_page_idx, sound_idx, sound_order_idx
 
@@ -209,6 +210,8 @@ def mouse_in(type, px, py, button):
     # print type, px, py, button
     if ZOOM_LEVELS[zoom_idx] == 'sound':
         sound_mouse(type, px, py, button)
+    elif ZOOM_LEVELS[zoom_idx] == 'rhythm':
+        rhythm_mouse(type, px, py, button)
 
 SOUND_ORDERINGS = ['time', 'cluster', 'similarity']
 sound_order_idx = 0
@@ -289,6 +292,13 @@ def paginate_sound():
                 page.append(base)
 
     print 'done paginate'
+
+def rhythm_mouse(type, px, py, button):
+    ngroups = len(rhythm_square.groups)
+    g_idx = int(py * ngroups)
+    if type == 'mouse-button-press' and button == 3 and ngroups > 0:
+        print "delete", g_idx
+        rhythm_square.groups.pop(g_idx)
 
 def sound_mouse(type, px, py, button):
     global sound_idx, sound_dragging, sound_selection, sound_dragging_first, zoom_idx, playseg, audio_frame
